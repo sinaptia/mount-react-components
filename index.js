07fe943886563c32df7758a9eda7b8e1a0bead27
Diego Pousa <diegopousalp@gmail.com>
2023-08-22 16:39:48 -0300

    Have a function that mount the component without events

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client'
+
+export function pureMountComponents (components) {
+  const roots = document.querySelectorAll('[data-component]')
+
+  Array.from(roots).forEach(root => {
+    const container = createRoot(root)
+    const props = JSON.parse(root.dataset.props || '{}')
+    const Component = components[root.dataset.component]
+
+    container.render(<Component {...props} />)
+  })
+}
 
 export function mountComponents (components) {
   document.addEventListener('DOMContentLoaded', () => {
-    const roots = document.querySelectorAll('[data-component]')
-
-    Array.from(roots).forEach(root => {
-      const container = createRoot(root);
-      const props = JSON.parse(root.dataset.props || '{}')
-      const Component = components[root.dataset.component]
-
-      container.render(<Component {...props} />);
-    })
+    pureMountComponents(components)
   })
 }
